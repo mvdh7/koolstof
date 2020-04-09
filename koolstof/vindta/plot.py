7aa4a74cba225e0f511c90fbafbf29e88e63660b
mvdh7 <m.p.humphreys@icloud.com>
2020-04-09 17:58:00 +0200

    Add bad_dic cutoffs

--- koolstof/vindta/plot.py
+++ koolstof/vindta/plot.py
@@ -9,10 +9,11 @@ def increments(ax, dbs, logfile, c='xkcd:navy', alpha=0.25, **kwargs):
     assert 'logfile_iloc' in dbs.columns, \
         'You must first run `ks.vindta.logfile2dbs()`.'
     fymax = 1.0
-    for i in dbs.logfile_iloc.values:
-        ax.plot('minutes', 'increments', data=logfile.table[i], c=c,
-                alpha=alpha, )
-        fymax = max([fymax, max(logfile.table[i]['increments'][-3:])])
+    for ix, i in enumerate(dbs.logfile_iloc.values):
+        if ~dbs.bad_dic.iloc[ix]:
+            ax.plot('minutes', 'increments', data=logfile.table[i], c=c,
+                    alpha=alpha, )
+            fymax = max([fymax, max(logfile.table[i]['increments'][-3:])])
     ax.set_xlim([0, dbs['run time'].max()])
     ax.set_ylim([0, fymax*1.25])
     ax.set_xlabel('Run time / minutes')
@@ -25,13 +26,13 @@ def blanks(ax, dbs, c='xkcd:navy', alpha=0.5, **kwargs):
     """
     assert 'blank_here' in dbs.columns, \
         'You must first run `ks.vindta.get_blanks()`.'
-    dbs.plot.scatter('analysisdate', 'blank_here', ax=ax, c=c, alpha=alpha,
-                     **kwargs)
+    dbs[~dbs.bad_dic].plot.scatter('analysisdate', 'blank_here', ax=ax, c=c,
+                                   alpha=alpha, **kwargs)
     ax.set_xlim([min(dbs.analysisdate) - timedelta64(3, 'h'),
                  max(dbs.analysisdate) + timedelta64(3, 'h')])
     ax.xaxis.set_major_locator(mdates.DayLocator())
     ax.xaxis.set_major_formatter(mdates.DateFormatter('%d'))
     ax.set_xlabel('Day of month')
-    ax.set_ylim([0, max(dbs.blank_here)*1.05])
-    ax.set_ylabel('Sample-by-sample blank')
+    ax.set_ylim([0, max(dbs[~dbs.bad_dic].blank_here)*1.05])
+    ax.set_ylabel('Sample blank / per minute')
     return ax
