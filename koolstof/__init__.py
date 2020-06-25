b08b158f34e08718ec1859724909290536804749
Matthew Humphreys <m.p.humphreys@icloud.com>
2020-06-25 10:39:32 +0200

    Prep for GitHub alpha release

--- koolstof/__init__.py
+++ koolstof/__init__.py
@@ -1,10 +1,12 @@
-"""Miscellaneous tools for marine carbonate chemistry."""
+"""Miscellaneous tools for marine carbonate chemistry and other delights."""
 
-import string
+import string, textwrap
 import numpy as np
 from . import crm, molar, spectro, vindta
 
 __all__ = ["crm", "molar", "spectro", "vindta"]
+
+__version__ = "0.0.2"
 
 
 lcletter = dict(zip(range(1, 27), string.ascii_lowercase))
@@ -14,3 +16,30 @@ def sigfig(x, sf):
     """Return `x` to `sf` significant figures."""
     factor = 10.0 ** np.ceil(np.log10(np.abs(x)))
     return factor * np.around(x / factor, decimals=sf)
+
+
+def say_hello():
+    greeting = textwrap.dedent(
+        r"""
+        ┌────────────────────────────┐
+
+           ┬┌─┌─┐┌─┐┬  ┌─┐┌┬┐┌─┐┌─┐
+           ├┴┐│ ││ ││  └─┐ │ │ │├┤ 
+           ┴ ┴└─┘└─┘┴─┘└─┘ ┴ └─┘└  
+        
+          Miscellaneous tools for
+          marine carbonate chemistry
+          and other delights
+        
+          Matthew P. Humphreys
+          https://mvdh.xyz
+        
+          Version {}
+
+
+        └────────────────────────────┘
+        """.format(
+            __version__
+        )
+    )
+    print(greeting)
--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
 import setuptools
+import koolstof as ks
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name="koolstof",
-    version="0.1.0",
+    version=ks.__version__,
     author="Humphreys, Matthew P.",
     author_email="m.p.humphreys@icloud.com",
-    description="Miscellaneous tools for marine carbonate chemistry",
+    description="Miscellaneous tools for marine carbonate chemistry and other delights",
     url="https://github.com/mvdh7/koolstof",
     packages=setuptools.find_packages(),
-    install_requires=["numpy>=1.14", "pandas>=1", "matplotlib>=3",],
+    install_requires=["numpy>=1.17", "pandas>=1", "matplotlib>=3.2",],
     long_description=long_description,
     long_description_content_type="text/markdown",
     classifiers=[
-        "Development Status :: 1 - Planning",
-        "Programming Language :: Python :: 3.7",
+        "Development Status :: 3 - Alpha",
+        "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
         "Natural Language :: English",
         "Intended Audience :: Science/Research",
+        "Topic :: Scientific/Engineering",
         "Topic :: Scientific/Engineering :: Chemistry",
+        "Topic :: Scientific/Engineering :: Visualization",
     ],
 )
