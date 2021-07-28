cc9cbed93076c075fff1b28248cc1567d3f3ad48
Louise Delaigue <louisedelaigue@gmail.com>
2021-07-28 14:32:15 +0200

    review changes

--- tests/test_optode.py
+++ tests/test_optode.py
@@ -1,17 +1,24 @@
 import koolstof as ks, pandas as pd
 
 # Import files
-spreadsheet = 'data/test_optode/spreadsheet.xlsx'
-text_files_folder_path = 'data/test_optode'
+spreadsheet = "tests/data/test_optode/spreadsheet.xlsx"
+text_files_folder_path = "tests/data/test_optode"
+
 
 def test_pH_optode():
     """Does ks.pH_optode() return a Pandas DataFrame?"""
     database = ks.pH_optode(spreadsheet, text_files_folder_path)
     assert isinstance(database, pd.DataFrame)
 
+
 df = ks.pH_optode(spreadsheet, text_files_folder_path)
 
-def optode_assertions(df):
+
+def test_optode_assertions():
     assert isinstance(df, pd.DataFrame)
     assert hasattr(df, "pH_s0_mean")
     assert hasattr(df, "filename")
+
+
+# test_pH_optode()
+# test_optode_assertions()
