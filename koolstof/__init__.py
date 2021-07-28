34c3bd63da627f6754e1df798cd698a50f81abe3
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2021-07-28 09:27:27 +0200

    To v0.0.18 and restyle

--- koolstof/__init__.py
+++ koolstof/__init__.py
@@ -2,7 +2,17 @@
 
 import string, textwrap
 import numpy as np
-from . import airica, crm, infrared, maps, molar, spectro, vindta, parameterisations, plot
+from . import (
+    airica,
+    crm,
+    infrared,
+    maps,
+    molar,
+    spectro,
+    vindta,
+    parameterisations,
+    plot,
+)
 from .vindta import (
     Dbs,
     read_dbs,
@@ -29,8 +39,8 @@ from .vindta import (
 from .plot import get_cluster_profile, cluster_profile
 from .parameterisations import aou_GG92
 
-__version__ = "0.0.17"
-__author__ = "Humphreys, Matthew P."
+__version__ = "0.0.18"
+__author__ = "Humphreys, Matthew P. and Delaigue, Louise"
 
 
 lcletter = dict(zip(range(1, 27), string.ascii_lowercase))
@@ -49,10 +59,10 @@ def say_hello():
         o  marine carbonate chemistry
         o  and other such things
         l  
-        s  Matthew P. Humphreys
-        t  https://mvdh.xyz
+        s  Version {}
+        t  doi:10.5281/zenodo.3999292
         o  
-        f  Version {}
+        f  https://humphreys.science
         """.format(
             __version__
         )
