2d59440a8b278ac86b1ffd09d6222413ab4a7ba0
Louise Delaigue <louisedelaigue@gmail.com>
2021-09-30 16:21:22 +0200

    added data + script for tests

---
+++ tests/test_airica.py
@@ -0,0 +1,26 @@
+import koolstof as ks, pandas as pd
+
+# Import files
+spreadsheet = "tests/data/test_airica/storage_test_Sept21.xlsx"
+dbs_filepath = "tests/data/test_airica/LD_storage_test.dbs"
+
+
+def test_airica():
+    """Does ks.airica.process_airica() return a Pandas DataFrame?"""
+    database = ks.airica.process_airica(2009.48,
+                                        spreadsheet,
+                                        dbs_filepath)
+    assert isinstance(database, pd.DataFrame)
+
+
+df = ks.airica.process_airica(2009.48,
+                              spreadsheet,
+                              dbs_filepath)
+
+def test_airica_assertions():
+    assert isinstance(df, pd.DataFrame)
+    assert hasattr(df, "TCO2_3")
+    assert hasattr(df, "filename")
+
+test_airica()
+test_airica_assertions()
