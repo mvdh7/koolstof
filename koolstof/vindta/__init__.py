e771e3f2873b8d459d1f51db8fd1a8bd11e54216
Matthew P. Humphreys <m.p.humphreys@icloud.com>
2023-03-29 16:00:10 +0200

    Enlarge docstring

--- koolstof/vindta/__init__.py
+++ koolstof/vindta/__init__.py
@@ -1,4 +1,39 @@
-"""Import and parse the data files produced by the Marianda VINDTA 3C."""
+"""
+koolstof.vindta
+===============
+
+Import, process and calibrate the DIC data files produced by the Marianda VINDTA 3C.
+
+Documentation is available online at https://koolstof.readthedocs.io/en/latest/vindta/
+
+Code examples assume that the import convention has been followed:
+
+  >>> import koolstof.vindta as ksv
+
+The following subfunctions are then available.
+
+Import data
+-----------
+
+    read_dbs
+    read_logfile
+
+Process and calibrate
+---------------------
+
+    blank_correction
+    calibrate_dic
+    poison_correction
+
+Data visualisation
+------------------
+
+    plot_increments
+    plot_blanks
+    plot_session_blanks
+    plot_dic_offset
+    plot_k_dic
+"""
 
 from .read import read_dbs, read_logfile
 from .get import blank_correction, calibrate_dic
