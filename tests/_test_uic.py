79d6951f1b4b274eb86189edde1fc2f064a3858f
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-06-27 15:37:07 +0200

    Plotting update

--- tests/_test_uic.py
+++ tests/_test_uic.py
@@ -3,7 +3,7 @@ import koolstof as ks
 
 
 dbs, logfile = ks.read_uic("tests/data/UIC-01302401.CSV", blank_from=9)
-ks.plot_increments(dbs, logfile)
+ks.plot_increments(dbs, logfile, use_from=9)  # TODO use_from vs blank_from
 ks.blank_per_measurement(dbs, logfile)
 sessions = ks.blank_per_session(dbs)
 ks.blank.counts_corrected(dbs, logfile=logfile, sessions=sessions)
