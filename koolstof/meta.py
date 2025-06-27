4f5c915e958f881fdce4332c8629b00408a4abd2
Matthew Humphreys <mvdh7@users.noreply.github.com>
2025-06-27 15:50:55 +0200

    Update for version beta 2

--- koolstof/meta.py
+++ koolstof/meta.py
@@ -1,16 +1,16 @@
 import textwrap
 
 
-__version__ = "1.0.0-b.1"
+__version__ = "1.0.0-b.2"
 __author__ = "Humphreys, Matthew P."
 
 
 def hello():
     greeting = textwrap.dedent(
         r"""
-        k  Miscellaneous tools for
-        o  marine carbonate chemistry
-        o  and other such things
+        k  Dissolved inorganic carbon
+        o  measurement processing
+        o  
         l  
         s  Version {}
         t  doi:10.5281/zenodo.3999292
